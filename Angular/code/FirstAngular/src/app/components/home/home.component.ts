79b09189ddb89912413ba5a51fbfa1ee4636f486
zhucunlong <zhucunlong@cug125.com>
2019-05-20 21:45:26 +0800

    在angular中使用原生或viewchild的方式操作dom

--- Angular/code/FirstAngular/src/app/components/home/home.component.ts
+++ Angular/code/FirstAngular/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,14 @@ export class HomeComponent implements OnInit {
   public approvalStatus: number = 3;
   public today: any = new Date();
   public modeltest: string = '默认值';
+  @ViewChild('test') box: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+    console.log(this.box.nativeElement.innerHTML);
   }
   handletest() {
     alert('hello angular');
