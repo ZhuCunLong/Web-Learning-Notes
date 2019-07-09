378c52fee960d2ea157376508279ccb8a7bc000b
zhucunlong <814026231@qq.com>
2019-07-09 16:38:01 +0800

    修改错字

---
+++ JS/code/js继承&原型链/00-例题.js
@@ -0,0 +1,20 @@
+function Foo() {
+	/*console.log(this)
+	console.log(this === Foo)*/
+	Foo.a = function() {
+		console.log(1)
+	}
+	/*this.a = function() {
+		console.log(2)
+	}*/
+}
+Foo.prototype.a = function() {
+	console.log(3)
+}
+/*Foo.a = function() {
+	console.log(4)
+}*/
+let obj = new Foo();
+obj.__proto__.a()
+obj.a();
+Foo.a();
