d7109883cac91fb56160de632124e9b40892716e
zhucunlong <814026231@qq.com>
2020-09-02 14:31:45 +0800

    update：添加了一种新的引入modules的方式

--- Vue/code/how-to-play-vuex/src/store/index.js
+++ Vue/code/how-to-play-vuex/src/store/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import user from './modules/user'
-import goods from './modules/goods'
+/* import user from './modules/user'
+import goods from './modules/goods' */
 
 import getters from '@/store/getters'
 
 Vue.use(Vuex)
+
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+const modulesFiles = require.context('./modules', true, /\.js$/)
+
+// you do not need `import app from './modules/app'`
+// it will auto require all vuex module from modules file
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
 
 const state = {
   outA: '外部state中的变量a',
@@ -51,10 +64,10 @@ const actions = { // 严格区分大小写
   }
 }
 
-const modules = {
+/* const modules = {
   user,
   goods
-}
+} */
 
 export default new Vuex.Store({
   state,
