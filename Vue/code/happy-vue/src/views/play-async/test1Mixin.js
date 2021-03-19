e9cee352a0a07baa1af2ff5805fc89a95b3bf723
zhucunlong <814026231@qq.com>
2021-03-19 17:46:10 +0800

    feat: 新增一种情况

--- Vue/code/happy-vue/src/views/play-async/test1Mixin.js
+++ Vue/code/happy-vue/src/views/play-async/test1Mixin.js
@@ -13,6 +13,29 @@ export default {
         this.error(err)
       }
       this.log('结束progress1-1')
+    },
+    async handleStart2 () {
+      this.log('启动2')
+      await this.progress2_1()
+      this.log('结束2')
+    },
+    async progress2_1 (flag = false) {
+      this.log('进入progress2-1')
+      if (flag) {
+        try {
+          await this.promise1('progress2-1', 2)
+        } catch (err) {
+          this.error(err)
+        }
+      } else {
+        await this.promise1('progress2-1', 2)
+      }
+      this.log('结束progress2-1')
+    },
+    handleStart3 () {
+      this.log('启动3')
+      this.progress2_1(true)
+      this.log('结束3')
     }
   }
 }
