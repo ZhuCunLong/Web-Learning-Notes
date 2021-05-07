8c5910b30c3cdf9132e53c2fdf792116960029a6
zhucunlong <814026231@qq.com>
2021-05-08 00:43:32 +0800

    feat: 1、算法；2、ts

---
+++ TypeScript/code/01-启动/01-first.js
@@ -0,0 +1,7 @@
+var obj = require('./02-module');
+function jspang() {
+    var web = "Hello World";
+    console.log(web);
+    console.log(obj.a);
+}
+jspang();
---
+++ TypeScript/code/01-启动/01-first.ts
@@ -0,0 +1,9 @@
+const obj = require('./02-module')
+
+function jspang(){
+  let web: string="Hello World"
+  console.log(web)
+  console.log(obj.a)
+}
+
+jspang()
