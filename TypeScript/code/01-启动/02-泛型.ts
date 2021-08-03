6aeda56a5b9b14459846e1c2ef7574066597ed84
zhucunlong <814026231@qq.com>
2021-08-03 11:10:55 +0800

    泛型

--- TypeScript/code/01-启动/02-泛型.ts
+++ TypeScript/code/01-启动/02-泛型.ts
@@ -1,11 +1,12 @@
 function getProperty<T, K>(obj: T, key: K) {
-    return obj[key];
+    console.log(obj, key)
+    return (<any>obj)[key]
 }
 
 let x = { a: 1, b: 2, c: 3, d: 4 };
 
-const a = getProperty(x, "a");
-const b = getProperty(x, "m");
+const a = getProperty<Object, String>(x, "a");
+const b = getProperty(x, "b");
 
 console.log(a, b)
 
