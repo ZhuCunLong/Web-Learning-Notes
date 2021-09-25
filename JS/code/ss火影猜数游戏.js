d6ab51fbd04767083fbe07a5ff258a3abf40d0ba
zhucunlong <814026231@qq.com>
2021-09-25 15:18:20 +0800

    feat: 写着玩

---
+++ JS/code/ss火影猜数游戏.js
@@ -0,0 +1,41 @@
+// const url= 'https://www.nowcoder.com/test/question/8b3c9b90fcb7476b97c671dfaaad2cbd?pid=27974724&tid=41286293'
+var readline = require('readline')
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal:false
+});
+
+let cur_line = 0
+let range = 0
+let min=0, max = 0
+let res = 0
+const handleProgress = () => {
+  res = parseInt((min + max) /2 )
+  console.log('中位数为', res)
+  console.log('请选择大还是小。大：1 小：0')
+}
+console.log('请输入范围')
+rl.on('line', function(line){
+	if(cur_line === 0){
+    range = Number(line)
+    max = range
+    cur_line ++ 
+    handleProgress()
+	} else if(cur_line > 0){
+    const choice = Number(line)
+    if(choice === 2){
+      cur_line = 0
+      min = 0,max = 0
+      console.log('请输入范围')
+      return 
+    }
+    if(choice === 0){
+      min = res
+    } else {
+      max = res
+    }
+    handleProgress()
+	}
+})
+
